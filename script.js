f997a462231ab9f5cf83b0010a5e6c77a5663a25
620220808-cpu <620220808@fa-hists.edu.eg>
2026-07-13 01:12:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,125 +1,68 @@
-let button = document.getElementById("clean");
+let button=document.getElementById("clean");
+let dust=document.getElementById("dust");
+let progress=document.getElementById("progress");
+let percent=document.getElementById("percent");
+let message=document.getElementById("message");
+let car=document.querySelector(".car");
 
-let dust = document.getElementById("dust");
+let value=0;
+let cleaning=false;
 
-let progress = document.getElementById("progress");
-
-let percent = document.getElementById("percent");
-
-let message = document.getElementById("message");
-
-let car = document.querySelector(".car");
-
-
-let value = 0;
-let cleaning = false;
-
-
-
-button.onclick = function(){
-
+button.onclick=function(){
 
 if(cleaning) return;
 
-cleaning = true;
+cleaning=true;
 
+let timer=setInterval(function(){
 
-let timer = setInterval(function(){
+value+=5;
 
+progress.style.width=value+"%";
+percent.innerHTML=value+"%";
 
-value += 5;
+/* إزالة التراب تدريجياً */
 
+dust.style.opacity=1-value/100;
 
-progress.style.width = value + "%";
-
-
-percent.innerHTML = value + "%";
-
-
-// إزالة التراب تدريجياً
-
-dust.style.opacity = 1 - value / 100;
-
-
-
-if(value >= 100){
-
+if(value>=100){
 
 clearInterval(timer);
 
-
-// إضافة لمعة للعربية
-
 car.classList.add("shine");
 
+message.style.display="block";
 
-// إظهار الرسالة
-
-message.style.display = "block";
-
-
-// إخفاء الزر
-
-button.style.display = "none";
-
-
-// تشغيل الكونفيتي
+button.style.display="none";
 
 createConfetti();
 
-
 }
-
 
 },200);
 
-
-
 }
-
-
-
 
 function createConfetti(){
 
+for(let i=0;i<120;i++){
 
-for(let i = 0; i < 100; i++){
+let piece=document.createElement("div");
 
+piece.className="confetti";
 
-let piece = document.createElement("div");
+piece.style.left=Math.random()*100+"vw";
 
+piece.style.background=
+"rgb("+
+Math.random()*255+","+
+Math.random()*255+","+
+Math.random()*255+")";
 
-piece.className = "confetti";
-
-
-piece.style.left =
-Math.random() * 100 + "vw";
-
-
-piece.style.top =
-"-20px";
-
-
-piece.style.background =
-"rgb("
-+ Math.random()*255 +
-","
-+ Math.random()*255 +
-","
-+ Math.random()*255 +
-")";
-
-
-piece.style.animationDuration =
-(2 + Math.random()*3) + "s";
-
-
+piece.style.animationDuration=(2+Math.random()*3)+"s";
 
 document.body.appendChild(piece);
 
-
-
 }
-
 
 }
